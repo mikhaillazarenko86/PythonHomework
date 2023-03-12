1d6b40aa9ca7c25c456c5f547a91b6706f31ee7c
Михаил Лазаренко <mihaillazarenko@192.168.0.8>
2023-03-12 11:53:37 +0300

    finished homework task 34

--- HWTask32.py
+++ HWTask32.py
@@ -4,6 +4,8 @@ data = list(map(int, input('Введите число: ').split()))
 min = int(input("Введите нижний предел: "))
 max = int(input("Введите верхний предел: "))
 print(data)
-for i in range(len(data)):
-    if data[i] > min and data[i] < max:
-        print(f'Индекс числa {data[i]} в диапазоне от {min} до {max} равен {i}')
+ for i in range(len(data)):
+     if data[i] > min and data[i] < max:
+         print(f'Индекс числa {data[i]} в диапазоне от {min} до {max} равен {i}')
+# res = list(filter(lambda x: x > min and x < max, data))
+# print(res)
